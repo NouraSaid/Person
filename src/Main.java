c43828d8bb1e9d0a802602aaf20c6b2b08c8ea71
Noura Said AL Saidi <97401813+NouraSaid@users.noreply.github.com>
2024-03-31 10:58:22 +0400

    area of rectangle

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class  Main{
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Creating a Rectangle object
+        Rectangle rectangle = new Rectangle(5.0, 4.0);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Calculating and printing area and perimeter
+        System.out.println("Area of the rectangle: " + rectangle.calculateArea());
+        System.out.println("Perimeter of the rectangle: " + rectangle.calculatePerimeter());
     }
 }
---
+++ src/Rectangle .java
@@ -0,0 +1,40 @@
+public class Rectangle {
+    // Private attributes
+    private double length;
+    private double width;
+
+    // Constructor
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    // Method to calculate the area of the rectangle
+    public double calculateArea() {
+        return length * width;
+    }
+
+    // Method to calculate the perimeter of the rectangle
+    public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+
+    // Getters and setters (optional)
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+
